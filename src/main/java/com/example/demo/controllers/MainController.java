cb6c81f702c7592323997732c9cc61b902283fb0
tellurixn <victor.babaryckin@yandex.ru>
2024-05-17 12:37:35 +0700

    Реализовал изменение цвета иконки в зависимости от состояния службы адаптера

--- src/main/java/com/example/demo/controllers/MainController.java
+++ src/main/java/com/example/demo/controllers/MainController.java
@@ -1,26 +1,55 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.io.IOException;
+import java.util.Scanner;
 
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String index(){
+    private final String serviceName = "AdapterSMEV";
 
+    @GetMapping("/")
+    public String index(Model model){
+        boolean isAdapterRunning = checkIfServiceRunning(serviceName);
+
+        model.addAttribute("isAdapterRunning", isAdapterRunning);
         return "index";
     }
 
     @GetMapping("/history")
-    public String history(){
+    public String history(Model model){
+        boolean isAdapterRunning = checkIfServiceRunning(serviceName);
 
+        model.addAttribute("isAdapterRunning", isAdapterRunning);
         return "history";
     }
 
     @GetMapping("/egr_zags")
-    public String egr_zags(){
+    public String egr_zags(Model model){
+        boolean isAdapterRunning = checkIfServiceRunning(serviceName);
 
+        model.addAttribute("isAdapterRunning", isAdapterRunning);
         return "EGR_ZAGS_Request";
+    }
+
+
+    public boolean checkIfServiceRunning(String serviceName) {
+        Process process;
+        try {
+            process = Runtime.getRuntime().exec("sc query " + serviceName);
+            Scanner reader = new Scanner(process.getInputStream(), "UTF-8");
+            while(reader.hasNextLine()) {
+                if(reader.nextLine().contains("RUNNING")) {
+                    return true;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
